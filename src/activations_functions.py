Fixed softmax by using the derivative function of the right layer

src/activations_functions.py
@@ -16,7 +16,7 @@ class Sigmoid(ActivationFunction):
 
     @staticmethod
     def activation_function(x):
-        return (1 / (1 + np.exp(-x)))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def derivative_function(x):
@@ -39,8 +39,7 @@ class Softmax(ActivationFunction):
     def activation_function(x):
         exps = np.exp(x)
         return exps / exps.sum()
-    
-    # @staticmethod
-    # def derivative_function(x):
-    #     s = x.reshape(-1, 1)
-    #     return np.diagflat(s) - np.dot(s, s.T)
+
+    @staticmethod
+    def derivative_function(x):
+        raise NotImplementedError("Using Softmax in an intermediate layer isn't supported yet")

src/ann.py
@@ -11,6 +11,7 @@ Layer = namedtuple("Layer", ["activation_function", "weights"])
 
 class ANN(object):
     EXTENSION = '.ann'
+
     def __init__(self):
         self.layers = list()
 
@@ -51,7 +52,8 @@ class ANN(object):
         for i in range(len(self.layers) - 2, -1, -1):
             curr_error = np.multiply(
                 self.layers[i + 1].weights.dot(layers_error[i + 1].transpose()).transpose(),
-                np.multiply(layers_output[i + 1], self.layers[i + 1].activation_function.derivative_function(layers_output[i + 1]))
+                np.multiply(layers_output[i + 1],
+                            self.layers[i].activation_function.derivative_function(layers_output[i + 1]))
             )
             layers_error[i] = curr_error
         